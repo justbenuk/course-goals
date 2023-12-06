157068346dd3b20c826a4df3f59cc722805932a6
justbenuk <justbenuk@gmail.com>
2023-12-06 13:01:58 +0000

    initial commit

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,19 +1,29 @@
+import { ReactNode } from "react"
 import { GoalList } from "../types"
 import CourseGoal from "./CourseGoal"
 import InfoBox from "./InfoBox"
-export default function CourseGoalList({goals, onDelete}: GoalList) {
-  if(goals.length === 0){
+export default function CourseGoalList({ goals, onDelete }: GoalList) {
+
+
+  if (goals.length === 0) {
     return <InfoBox mode="hint">You have no course goals yet, Why dont you add some!</InfoBox>
   }
-  return (
-    <ul>
-        {goals.map(goal => (
-          <li key={goal.id}>
-            <CourseGoal id={goal.id} title={goal.title} onDelete={onDelete}>
-              <p>{goal.description}</p>
-            </CourseGoal>
-          </li>
-        ))}
-      </ul>
+  let warningBox: ReactNode
+
+  if (goals.length >= 4) {
+    warningBox = <InfoBox mode='warning'>You have have set a lot of goals, don't put too much on your plate! </InfoBox>
+  } return (
+    <>
+      {warningBox}
+        <ul>
+          {goals.map(goal => (
+            <li key={goal.id}>
+              <CourseGoal id={goal.id} title={goal.title} onDelete={onDelete}>
+                <p>{goal.description}</p>
+              </CourseGoal>
+            </li>
+          ))}
+        </ul>
+    </>
   )
 }
